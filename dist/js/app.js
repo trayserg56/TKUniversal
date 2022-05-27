692e9aad65e1d6ba06d83a8d7467492047f5a2eb
trayserg56 <trayserg@gmail.com>
2022-05-28 01:50:25 +0500

    28.05

--- dist/js/app.js
+++ dist/js/app.js
@@ -19,6 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
 		dots: false,
 		infinite: false,
 		arrows: false,
+		responsive: [
+			{
+			breakpoint: 1000,
+			settings: "unslick"
+			}
+			]
 	});
 	$('.about__video').slick({
 		slidesToShow: 3,
@@ -26,5 +32,21 @@ document.addEventListener('DOMContentLoaded', () => {
 		dots: false,
 		infinite: false,
 		arrows: false,
+	});
+	$(".mobilefilter__offer").click(function(){
+		$(".mobile__items").removeClass("none");
+		$(".mobile__items").addClass("block");
+	});
+	$(".mobile__filter__back").click(function(){
+		$(".mobile__items").removeClass("block");
+		$(".mobile__items").addClass("none");
+	});
+	$(".header__mobile__burger").click(function(){
+		$(".header__mobile").addClass("nonemobile");
+		$(".header__mobile__content").removeClass("nonemobile");
+	});
+	$(".header__mobile__close").click(function(){
+		$(".header__mobile__content").addClass("nonemobile");
+		$(".header__mobile").removeClass("nonemobile");
 	});
 })
